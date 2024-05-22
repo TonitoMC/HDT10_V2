f6d746b097c2d6fdd4e41751f8ca04f904d548be
tonitomc <joseameridac@gmail.com>
2024-05-21 19:21:56 -0600

    Implementado Floyd y DirectedGraph

--- src/main/java/uvg/edu/gt/DirectedGraph.java
+++ src/main/java/uvg/edu/gt/DirectedGraph.java
@@ -1,2 +1,65 @@
-package uvg.edu.gt;public class DirectedGraph {
+package uvg.edu.gt;
+
+import java.util.*;
+
+public class DirectedGraph {
+    private int[][] adjacencyMatrix;
+    private int[][] floydMatrix;
+    private ArrayList<String> nodeList;
+
+    public DirectedGraph(int numNodes){
+        adjacencyMatrix = new int[numNodes][numNodes];
+        for (int i = 0; i < numNodes; i++){
+            Arrays.fill(adjacencyMatrix[i], Integer.MAX_VALUE);
+        }
+        nodeList = new ArrayList<String>();
+    }
+    public ArrayList<String> getNodeList(){
+        return nodeList;
+    }
+    public int[][] getAdjacencyMatrix(){
+        return adjacencyMatrix;
+    }
+    public void addNode(String node){
+        if (!nodeList.contains(node)){
+            nodeList.add(node);
+        }
+    }
+    public void addEdge(String fromNode, String toNode, int weight){
+        int fromIndex = nodeList.indexOf(fromNode);
+        int toIndex = nodeList.indexOf(toNode);
+        if (fromIndex != -1 && toIndex != -1){
+            adjacencyMatrix[fromIndex][toIndex] = weight;
+        }
+    }
+    public void removeEdge(String fromNode, String toNode){
+        int fromIndex = nodeList.indexOf(fromNode);
+        int toIndex = nodeList.indexOf(toNode);
+        if (fromIndex != -1 && toIndex != -1){
+            adjacencyMatrix[fromIndex][toIndex] = Integer.MAX_VALUE;
+        }
+    }
+    private void updateFloyd(){
+        int numNodes = nodeList.size();
+        floydMatrix = new int[numNodes][numNodes];
+        for (int i = 0; i < numNodes; i++){
+            for (int j = 0; j < numNodes; j++){
+                floydMatrix[i][j] = adjacencyMatrix[i][j];
+            }
+        }
+        //Recorre todos los nodos tratandolos como nodos intermedios
+        for (int k = 0; k < numNodes; k++){
+            for (int i = 0; i < numNodes; i++){
+                for (int j = 0; j < numNodes; j++){
+                    if (floydMatrix[i][k] != Integer.MAX_VALUE && floydMatrix[k][j] != Integer.MAX_VALUE &&
+                    floydMatrix[i][k] + floydMatrix[k][j]< floydMatrix[i][j]){
+                        floydMatrix[i][j] = floydMatrix[i][k] + floydMatrix[k][j];
+                    }
+                }
+            }
+        }
+    }
+    private String findCenter(){
+        return "1";
+    }
 }
--- src/test/java/uvg/edu/gt/DirectedGraphTest.java
+++ src/test/java/uvg/edu/gt/DirectedGraphTest.java
@@ -1,4 +1,50 @@
+package uvg.edu.gt;
+
 import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class DirectedGraphTest extends TestCase {
-  
+
+    public void testGetNodeList() {
+    }
+
+    public void testGetAdjacencyMatrix() {
+    }
+
+    public void testAddNode() {
+        DirectedGraph graph = new DirectedGraph(2);
+        graph.addNode("Guatemala");
+        ArrayList<String> nodeList = graph.getNodeList();
+        assertEquals(nodeList.size(), 1);
+        assertEquals(nodeList.get(0), "Guatemala");
+    }
+
+    public void testAddEdge() {
+        DirectedGraph graph = new DirectedGraph(3);
+        graph.addNode("Guatemala");
+        graph.addNode("Peten");
+        graph.addNode("Zacapa");
+        graph.addEdge("Guatemala", "Peten", 300);
+        graph.addEdge("Guatemala", "Zacapa", 400);
+        graph.addEdge("Peten", "Zacapa", 200);
+        int[][] matrix = graph.getAdjacencyMatrix();
+        assertEquals(matrix[0][1],300);
+        assertEquals(matrix[0][2],400);
+        assertEquals(matrix[1][2],200);
+    }
+
+    public void testRemoveEdge() {
+        DirectedGraph graph = new DirectedGraph(3);
+        graph.addNode("Guatemala");
+        graph.addNode("Peten");
+        graph.addNode("Zacapa");
+        graph.addEdge("Guatemala", "Peten", 300);
+        graph.addEdge("Guatemala", "Zacapa", 400);
+        graph.addEdge("Peten", "Zacapa", 200);
+        graph.removeEdge("Peten", "Zacapa");
+        int[][] matrix = graph.getAdjacencyMatrix();
+        assertEquals(matrix[1][2],Integer.MAX_VALUE);
+    }
 }
